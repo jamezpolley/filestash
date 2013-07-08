0557f7dfb8206948a253cf64104871ffd8d56f43
James Polley <jpolley@atlassian.com>
2013-07-09 08:21:34 +1000

    First stab at a FileBucket.class

--- file_index.py
+++ file_index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from functools import total_ordering
+import collections
 import datetime
 import hashlib
 import os
@@ -52,30 +54,46 @@ class File(object):
         self.md5, self.sha1 = hashes(open(file_path),
                                      (hashlib.md5(), hashlib.sha1()))
 
-    def __repr__(self):
+    def __str__(self):
         """Simple string representing the file.  """
+        return "%(sha1)s %(file_name)s" % self.__dict__
 
-        repr = "%(collection_date)s %(file_name)s: %(md5)s %(sha1)s %(stat)s" % self.__dict__
-        return repr
+class FileBucket(dict):
+    """Bucket for storing Files.
+
+    It's a multidict. Like a dict but values are stored in lists to handle duplicates.
+    """
+
+    def __setitem__(self, key, item):
+        """@todo: Docstring for __setitem__
+
+        :arg1: @todo
+        :returns: @todo
+        """
+
+        self.setdefault(key, []).append(item)
+
+
 
 def main():
     tag = sys.argv[1]
     file_path = sys.argv[2]
 
-    all_files = {}
+    fb = FileBucket()
 
     if os.path.isdir(file_path):
         for root, dirs, files in os.walk(file_path):
             base = os.path.abspath(root)
             for file in files:
                 full_path = os.path.join(base, file)
-                all_files[full_path] = File(tag, full_path)
+                f = File(tag, full_path)
+                fb[f.sha1] = f
     else:
         full_path = os.path.abspath(file_path)
-        myfile = File(tag, full_path)
-        all_files[full_path] = myfile
+        f = File(tag, full_path)
+        fb[f.sha1] = f
 
-    print yaml.dump(all_files)
+    print yaml.dump(fb)
 
 if __name__ == '__main__':
     main()
--- tests/test_file_index.py
+++ tests/test_file_index.py
@@ -31,7 +31,7 @@ class TestFileRead(unittest.TestCase):
     """Read file; check attributes; hash"""
 
     def setUp(self):
-        in_file= open("tests/test_file")
+        in_file= open("tests/data/test_file_1")
         self.md5 = "0035e1010ea5e875c8776c69dd0ee03e"
         self.sha1 = "f5c13d47495264cfa13f6d46ec52e7ad42474e53"
         hashers = [ hashlib.md5(), hashlib.sha1() ]
@@ -45,6 +45,30 @@ class TestFileRead(unittest.TestCase):
 
     def test_sha1(self):
         self.assertEqual(self.sha1, self.digests[1])
+
+class TestFileBucketDuplicateHandling(unittest.TestCase):
+    """Hash some files, add them to the bucket, retrieve details"""
+
+    def setUp(self):
+        self.fb = file_index.FileBucket()
+        self.key = "f5c13d47495264cfa13f6d46ec52e7ad42474e53"
+        self.test_filenames = ["test_file_1", "test_file_2"]
+        for filename in self.test_filenames:
+            f = file_index.File('tag', "tests/data/%s" % filename)
+            self.fb[f.sha1] = f
+
+    def tearDown(self):
+        pass
+
+    def test_fb_length(self):
+        self.assertEqual(len(self.fb), 1)
+
+    def test_fb_list_length(self):
+        self.assertEqual(len(self.fb[self.key]), 2)
+
+    def test_fb_list_items(self):
+        for f in self.fb[self.key]:
+            self.assertIn(f.file_name, self.test_filenames)
 
 if __name__ == '__main__':
         unittest.main()
